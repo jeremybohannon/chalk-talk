555691cfbd0ae5fe43ebeda2bdafcfd0b82474d9
JeremyBoh <jeremybohannon@outlook.com>
2017-03-23 00:15:28 -0400

    Homework 3

---
+++ index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+    document.getElementById('form').addEventListener('submit', function(){
+        alert('The form has been submitted and stored!')
+    });
+});
