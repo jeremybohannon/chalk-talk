2765a002d5602fe32820facf5a982647b26021dd
Jeremy Bohannon <jeremybohannon@Command.local>
2017-04-17 02:27:37 -0400

    Added gallery content'

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-window.addEventListener('load', function () {
-    document.getElementById('form').addEventListener('submit', function(){
-        alert('The form has been submitted and stored!')
-    });
-});
+$(document).ready(function(){
+  var images = $('.mySlides');
+  var dots = $('.dot');
+
+  var imageIndex = 1;
+
+  var defaultColor = 'rgba(30, 140, 150, 0.23)';
+  var highlightColor = '#00b7c8';
+
+  var numOfImages = images.length;
+
+  showImage(imageIndex);
+
+  $('#leftButton').click(function(){
+    showImage(imageIndex -= 1);
+  });
+
+  $('#rightButton').click(function(){
+    showImage(imageIndex += 1);
+  });
+
+  function showImage(newIndex){
+    if (newIndex > numOfImages) {imageIndex = 1; newIndex = 1;}
+    if (newIndex < 1) {imageIndex = numOfImages; newIndex = numOfImages}
+
+    imageIndex = newIndex;
+
+    for (i = 0; i < numOfImages; i++) {
+       images[i].style.display = 'none';
+       dots[i].style.backgroundColor = defaultColor;
+    }
+
+    images[newIndex - 1].style.display = 'block';
+    dots[newIndex - 1].style.backgroundColor = highlightColor
+  }
+
+
+  $('.dot').hover(function(){
+    var currentDot = $(this);
+    showImage(parseInt(currentDot.attr('id')));
+  });
+
+})
